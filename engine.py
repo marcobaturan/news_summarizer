edffc4318f52240d61c6bd555ea864775cd6050c
marco <marco.baturan@gmail.com>
2025-01-23 17:20:16 +0100

    Add proxy server to avoid google spam bot

--- engine.py
+++ engine.py
@@ -21,6 +21,14 @@ import logging
 from urllib.parse import urljoin, urlparse
 
 
+# cookies
+cookies = {'SOCS': 'CAISHAgCEhJnd3NfMjAyNDEyMDMtMF9SQzEaAmVzIAEaBgiA2sO6Bg',
+        'SOCS': 'CAISHAgCEhJnd3NfMjAyNDEyMDMtMF9SQzEaAmVzIAEaBgiA2sO6Bg',
+        'SNID':'AFAt11qAYTOVrf4Jw2K-4zC_wGRwtEOnVzFt4C0g'
+        }
+
+
+
 def api_call(api_key):
     # instance object Groq
     client = Groq(
@@ -271,7 +279,7 @@ def yt_method(url_youtube: str, llm_api_key: str, language: str, selected_limit:
         id_video = get_youtube_video_id(url_youtube)
 
         # Retrieve the transcript of the video in the specified language
-        json = YouTubeTranscriptApi.get_transcript(id_video, languages=['es','en','fr','de','it','hr','pt'], proxies={'http': '198.23.239.134:6540:cgirlzeq:bhlduner1c3x'})
+        json = YouTubeTranscriptApi.get_transcript(id_video, languages=['es','en','fr','de','it','hr','pt'], proxies={'http': '198.23.239.134:6540:cgirlzeq:bhlduner1c3x'}, cookies=cookies)
         time.sleep(3) # avoid overload google service
         # Extract phrases and concatenate them into a single string
         text = extract_phrases_and_concatenate(json)

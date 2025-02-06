9ee8b23316f1213b0d0486f6aa3cadaf37cbd412
marco <marco.baturan@gmail.com>
2025-02-06 20:12:53 +0100

    add try except error

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade
+sudo apt install software-properties-common apt-transport-https ca-certificates curl -y
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update
+sudo apt install google-chrome-stable
